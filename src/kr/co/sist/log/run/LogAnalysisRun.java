e5bc962b6c3b38f2e521a5f9f8b71e1c27a94f79
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-24 09:08:47 +0900

    빠른 작업을 위한 Login 생략

--- src/kr/co/sist/log/run/LogAnalysisRun.java
+++ src/kr/co/sist/log/run/LogAnalysisRun.java
@@ -6,6 +6,9 @@ import kr.co.sist.log.view.SelectLog;
 public class LogAnalysisRun {
 
 	public static void main(String[] args) {
+		//////// 12-24 영근 //////////////
+		// 빠른 작업을 위해 Login 창 막음
 		new SelectLog(new Login());
+		/////////////////////////////////
 	}
 }
