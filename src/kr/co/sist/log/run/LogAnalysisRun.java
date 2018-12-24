9662232146d13ab5bd17e1036f71373757af71d6
younggeun0 <34850791+younggeun0@users.noreply.github.com>
2018-12-24 17:58:03 +0900

    로그인 기능 복구

--- src/kr/co/sist/log/run/LogAnalysisRun.java
+++ src/kr/co/sist/log/run/LogAnalysisRun.java
@@ -1,14 +1,10 @@
 package kr.co.sist.log.run;
 
 import kr.co.sist.log.view.Login;
-import kr.co.sist.log.view.SelectLog;
 
 public class LogAnalysisRun {
 
 	public static void main(String[] args) {
-		//////// 12-24 영근 //////////////
-		// 빠른 작업을 위해 Login 창 막음
-		new SelectLog(new Login());
-		/////////////////////////////////
+		new Login();
 	}
 }
