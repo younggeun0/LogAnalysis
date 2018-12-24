3bb61fc5bc7387727e8daa6cd4023a7ea229c718
jeongmipark94 <45455409+jeongmipark94@users.noreply.github.com>
2018-12-24 14:53:21 +0900

    Update SelectLogEvt.java

--- src/kr/co/sist/log/evt/SelectLogEvt.java
+++ src/kr/co/sist/log/evt/SelectLogEvt.java
@@ -234,8 +234,12 @@ public class SelectLogEvt implements ActionListener {
 //////////////////////12.22 브라우저 카운터, mapBrowser에 저장 구현 끝 (선의)////////////
 
 	public void countHttpStatusCode(String temp) {
-		// 3과 6번 서비스를 성공수행(200)한 횟수, 실패 횟수(404), 비정상을 누적합시킴(code200, code404, code403)
+		// 3과 5번 서비스를 성공수행(200)한 횟수, 실패 횟수(404), 비정상을 누적합시킴(code200, code404, code403)
 		// 비율은 calCode403Share()에 구현
+//		if() {
+//			
+//		}
+		
 	}
 
 	public void countRequestHour(String temp) {
