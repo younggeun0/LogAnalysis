8b59ff3be0b9393c495902614808d9a52f529e49
Young Oh <34850791+younggeun0@users.noreply.github.com>
2018-12-25 22:20:06 +0900

    변수명 변경

--- src/kr/co/sist/log/view/Result.java
+++ src/kr/co/sist/log/view/Result.java
@@ -19,21 +19,21 @@ import kr.co.sist.log.evt.SelectMenuEvt;
 
 
 /////////////// 12-22 Result JDialog구현, Event처리(정미) /////////////////////
-// //////////// 12-24 Result 디자인 변경(영근), sle로부터 출력 결과 설정 //////
+// //////////// 12-24 Result 디자인 변경(영근), sme로부터 출력 결과 설정 //////
 @SuppressWarnings("serial")
 public class Result extends JDialog {
 	
-	private SelectMenuEvt sle;
+	private SelectMenuEvt sme;
 	
 	public JPanel browserInfo() {
 		
 		JPanel jpBrowser = new JPanel();
 		setLayout(new GridLayout(1, 5));
 		
-		JLabel[] jlBrowser = new JLabel[sle.getBrowser().length];
+		JLabel[] jlBrowser = new JLabel[sme.getBrowser().length];
 		
-		Map<String, Integer> mapBrowser = sle.getMapBrowser();
-		Map<String, String> mapBrowserShare = sle.getMapBrowserShare();
+		Map<String, Integer> mapBrowser = sme.getMapBrowser();
+		Map<String, String> mapBrowserShare = sme.getMapBrowserShare();
 		
 		StringBuilder content = new StringBuilder();
 		
@@ -55,43 +55,43 @@ public class Result extends JDialog {
 		return jpBrowser;
 	}
 
-	public Result(SelectMenuEvt sle, SelectMenu sl) {
+	public Result(SelectMenuEvt sme, SelectMenu sl) {
 		super(sl, "결과 출력",true);
-		this.sle = sle;
+		this.sme = sme;
 		
 		JPanel pnNo = new JPanel();
 		JPanel pnCe= new JPanel();
 		JButton jbConfirm = new JButton("확인"); 
 		jbConfirm.setFont(new Font(Font.DIALOG, Font.BOLD, 25));
 		
-		File reportFile = new File(sle.getFilePath());
+		File reportFile = new File(sme.getFilePath());
 		
 		reportFile.lastModified();
 		pnNo.add(new JButton("파일명"));
 		pnNo.add(new JLabel(reportFile.getName()));
 		pnNo.add(new JButton("생성된 날짜"));
-		pnNo.add(new JLabel(sle.getLogTxtCreationDate()));
+		pnNo.add(new JLabel(sme.getLogTxtCreationDate()));
 		pnCe.add(new JButton("1. 최다 사용 key의 이름과 횟수 : "));
-		pnCe.add(new JLabel(sle.getMostFrequentKey()+" : "+sle.getMapKey().get(sle.getMostFrequentKey())+"번"));
+		pnCe.add(new JLabel(sme.getMostFrequentKey()+" : "+sme.getMapKey().get(sme.getMostFrequentKey())+"번"));
 		pnCe.add(new JButton("2. 브라우저별 접속횟수, 비율"));
 		pnCe.add(browserInfo()); 
 		pnCe.add(new JButton("3. 서비스 성공(200) 실패(404) 횟수"));
-		pnCe.add(new JLabel("성공(200) : "+sle.getCode200()+", 실패(404) : "+sle.getCode404()));
+		pnCe.add(new JLabel("성공(200) : "+sme.getCode200()+", 실패(404) : "+sme.getCode404()));
 		pnCe.add(new JButton("4. 요청이 가장 많은 시간"));
-		pnCe.add(new JLabel(sle.getMostFrequentHour()+"시"));
+		pnCe.add(new JLabel(sme.getMostFrequentHour()+"시"));
 		pnCe.add(new JButton("5. 비정상 요청 횟수와 비율"));
-		pnCe.add(new JLabel(sle.getCode403()+" ("
+		pnCe.add(new JLabel(sme.getCode403()+" ("
 				+ String.format("%4.2f", 
-						sle.getCode403()/(double)sle.getRequestNum()*100)
+						sme.getCode403()/(double)sme.getRequestNum()*100)
 				+"%)"));
-		if (sle.getStart() == 0 && sle.getEnd() == 0) {
-			pnCe.add(new JButton("6. "+(sle.getStart()+1)+"~"+sle.getRequestNum()+" 라인 가장 빈도수가 높은 key와 횟수"));
-			pnCe.add(new JLabel(sle.getMostFrequentKey()+" : "
-					+sle.getMapKey().get(sle.getMostFrequentKey())+"번"));
+		if (sme.getStart() == 0 && sme.getEnd() == 0) {
+			pnCe.add(new JButton("6. "+(sme.getStart()+1)+"~"+sme.getRequestNum()+" 라인 가장 빈도수가 높은 key와 횟수"));
+			pnCe.add(new JLabel(sme.getMostFrequentKey()+" : "
+					+sme.getMapKey().get(sme.getMostFrequentKey())+"번"));
 		} else {
-			pnCe.add(new JButton("6. "+sle.getStart()+"~"+sle.getEnd()+" 라인 가장 빈도수가 높은 key와 횟수"));
-			pnCe.add(new JLabel(sle.getMostFrequentKeyBetweenStartAndEnd()+" : "
-					+sle.getMapKeyBetweenStartAndEnd().get(sle.getMostFrequentKeyBetweenStartAndEnd())+"번"));
+			pnCe.add(new JButton("6. "+sme.getStart()+"~"+sme.getEnd()+" 라인 가장 빈도수가 높은 key와 횟수"));
+			pnCe.add(new JLabel(sme.getMostFrequentKeyBetweenStartAndEnd()+" : "
+					+sme.getMapKeyBetweenStartAndEnd().get(sme.getMostFrequentKeyBetweenStartAndEnd())+"번"));
 		}
 		pnNo.setLayout(new GridLayout(1, 4));
 		pnCe.setLayout(new GridLayout(6, 2));
