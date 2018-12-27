bebd809544b53f93024e3d59996d3f8c5aa92f63
jeongmipark94 <45455409+jeongmipark94@users.noreply.github.com>
2018-12-27 15:02:30 +0900

    te

--- src/kr/co/sist/log/view/Result.java
+++ src/kr/co/sist/log/view/Result.java
@@ -8,10 +8,13 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import kr.co.sist.log.evt.ResultEvt;
 import kr.co.sist.log.evt.SelectMenuEvt;
@@ -24,6 +27,7 @@ public class Result extends JDialog {
 	
 	private SelectMenuEvt sme;
 	
+	ImageIcon icon = new ImageIcon("C:\\Users\\owner\\Desktop\\design\\reviewIMG.jpg");
 	public JPanel browserInfo() {
 		
 		JPanel jpBrowser = new JPanel();
@@ -60,46 +64,50 @@ public class Result extends JDialog {
 		
 		JPanel pnNo = new JPanel();
 		JPanel pnCe= new JPanel();
-		JButton jbConfirm = new JButton("확인"); 
+		JButton jbConfirm = new JButton("확인");
 		jbConfirm.setFont(new Font(Font.DIALOG, Font.BOLD, 25));
+	
 		
 		File reportFile = new File(sme.getFilePath());
 		
 		reportFile.lastModified();
-		pnNo.add(new JButton("파일명"));
-		pnNo.add(new JLabel(reportFile.getName()));
-		pnNo.add(new JButton("생성된 날짜"));
-		pnNo.add(new JLabel(sme.getLogTxtCreationDate()));
-		pnCe.add(new JButton("1. 최다 사용 key의 이름과 횟수"));
-		pnCe.add(new JLabel(sme.getMostFrequentKey()+" : "+sme.getMapKey().get(sme.getMostFrequentKey())+"번"));
-		pnCe.add(new JButton("2. 브라우저별 접속횟수, 비율"));
-		pnCe.add(browserInfo()); 
-		pnCe.add(new JButton("3. 서비스 성공(200) 실패(404) 횟수"));
-		pnCe.add(new JLabel("성공(200) : "+sme.getCode200()+", 실패(404) : "+sme.getCode404()));
-		pnCe.add(new JButton("4. 요청이 가장 많은 시간"));
-		pnCe.add(new JLabel(sme.getMostFrequentHour()+"시"));
-		pnCe.add(new JButton("5. 비정상 요청 횟수와 비율"));
-		pnCe.add(new JLabel(sme.getCode403()+" ("
+		add(new JLabel(icon)).setBounds(30, 10, 100, 50);
+		add(new JTextField(reportFile.getName())).setBounds(125, 10, 100, 30);
+		add(new JLabel("생성된 날짜")).setBounds(320, 10, 100, 30);
+		add(new JTextField(sme.getLogTxtCreationDate())).setBounds(430, 10, 150, 30);
+		add(new JLabel("1. 최다 사용 key의 이름과 횟수")).setBounds(30, 50, 200, 50);
+		add(new JTextField(sme.getMostFrequentKey()+" : "+sme.getMapKey().get(sme.getMostFrequentKey())+"번")).setBounds(320, 60, 200, 30);
+		add(new JLabel("2. 브라우저별 접속횟수, 비율")).setBounds(30, 130, 200, 50);
+		add(browserInfo()).setBounds(300, 100, 200, 70); 
+		add(new JLabel("3. 서비스 성공(200) 실패(404) 횟수")).setBounds(30, 230, 200, 50);
+		add(new JTextField("성공(200) : "+sme.getCode200()+", 실패(404) : "+sme.getCode404())).setBounds(300, 230, 200, 50);
+		add(new JLabel("4. 요청이 가장 많은 시간")).setBounds(30, 310, 200, 30);
+		add(new JTextArea(sme.getMostFrequentHour()+"시")).setBounds(300	, 310, 200,30);
+		add(new JLabel("5. 비정상 요청 횟수와 비율")).setBounds(30, 360, 200, 30);
+		add(new JLabel(sme.getCode403()+" ("
 				+ String.format("%4.2f", 
 						sme.getCode403()/(double)sme.getRequestNum()*100)
-				+"%)"));
+				+"%)")).setBounds(300, 360, 200, 30);
 		if (sme.getStart() == 0 && sme.getEnd() == 0) {
-			pnCe.add(new JButton("6. "+(sme.getStart()+1)+"~"+sme.getRequestNum()+" 라인 가장 빈도수가 높은 key와 횟수"));
-			pnCe.add(new JLabel(sme.getMostFrequentKey()+" : "
+			pnCe.add(new JTextField("6. "+(sme.getStart()+1)+"~"+sme.getRequestNum()+" 라인 가장 빈도수가 높은 key와 횟수"));
+			pnCe.add(new JTextField(sme.getMostFrequentKey()+" : "
 					+sme.getMapKey().get(sme.getMostFrequentKey())+"번"));
 		} else {
-			pnCe.add(new JButton("6. "+sme.getStart()+"~"+(sme.getEnd() > sme.getRequestNum() ? sme.getRequestNum() : sme.getEnd())
+			pnCe.add(new JTextField("6. "+sme.getStart()+"~"+(sme.getEnd() > sme.getRequestNum() ? sme.getRequestNum() : sme.getEnd())
 					+" 라인 가장 빈도수가 높은 key와 횟수"));
-			pnCe.add(new JLabel(sme.getMostFrequentKeyBetweenStartAndEnd()+" : "
+			pnCe.add(new JTextField(sme.getMostFrequentKeyBetweenStartAndEnd()+" : "
 					+sme.getMapKeyBetweenStartAndEnd().get(sme.getMostFrequentKeyBetweenStartAndEnd())+"번"));
 		}
-		pnNo.setLayout(new GridLayout(1, 4));
-		pnCe.setLayout(new GridLayout(6, 2));
-		setLayout(new BorderLayout());
+		add(jbConfirm).setBounds(325, 480, 70, 50);
+//		pnNo.setLayout(new GridLayout(1, 4));
+//		pnCe.setLayout(new GridLayout(6, 2));
+//		setLayout(new BorderLayout());
+		setLayout(null);
 		
-		add("North",pnNo);
-		add("Center",pnCe);
-		add("South",jbConfirm);
+		
+//		add("North",pnNo);
+//		add("Center",pnCe);
+//		add("South",jbConfirm);
 		
 		ResultEvt r = new ResultEvt(this);
 		jbConfirm.addActionListener(r);
--- src/kr/co/sist/log/view/SelectMenu.java
+++ src/kr/co/sist/log/view/SelectMenu.java
@@ -1,12 +1,17 @@
 package kr.co.sist.log.view;
 
+import java.awt.Graphics;
+import java.awt.ScrollPane;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
 
@@ -20,21 +25,32 @@ public class SelectMenu extends JDialog {
 	private JButton jbLineView;
 	private JTextField jtStart;
 	private JTextField jtEnd;
-	
+	JScrollPane scrollPane;
+	ImageIcon icon;
+
 	public SelectMenu(JFrame login) {
 		super(login, "Log Analysis App", true);
 		
+		icon=new ImageIcon("C:\\Users\\owner\\Desktop\\결과창\\메뉴선택.jpg");
+		JPanel background = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(),0,0,null);
+				super.paintComponents(g);
+			}
+		};
+		scrollPane=new JScrollPane(background);setContentPane(scrollPane);
+
 		JLabel jlStart = new JLabel("시작");
 		JLabel jlEnd = new JLabel("끝");
-		
+
 		jbView = new JButton("View");
 		jbReport = new JButton("Report");
 		jbLineView = new JButton("LineView");
 		jtStart = new JTextField();
 		jtEnd = new JTextField();
-		
+
 		setLayout(null);
-		
+
 		jbView.setBounds(40, 30, 150, 50);
 		add(jbView);
 		jbReport.setBounds(220, 30, 150, 50);
@@ -45,19 +61,21 @@ public class SelectMenu extends JDialog {
 		add(jtStart);
 		jtEnd.setBounds(95, 140, 150, 30);
 		add(jtEnd);
-		jlStart.setBounds(55,100,50,30);
+		jlStart.setBounds(55, 100, 50, 30);
 		add(jlStart);
-		jlEnd.setBounds(55,140,50,30);
+		jlEnd.setBounds(55, 140, 50, 30);
 		add(jlEnd);
-		
+
 		SelectMenuEvt sme = new SelectMenuEvt(this);
 		jbView.addActionListener(sme);
 		jbReport.addActionListener(sme);
 		jbLineView.addActionListener(sme);
 		jbLineView.setActionCommand("lineView");
-		jtStart.registerKeyboardAction(sme, "lineView", KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), JComponent.WHEN_FOCUSED);
-		jtEnd.registerKeyboardAction(sme, "lineView", KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), JComponent.WHEN_FOCUSED);
-		
+		jtStart.registerKeyboardAction(sme, "lineView", KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
+				JComponent.WHEN_FOCUSED);
+		jtEnd.registerKeyboardAction(sme, "lineView", KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
+				JComponent.WHEN_FOCUSED);
+
 		setBounds(400, 300, 420, 282);
 		setResizable(false);
 		setVisible(true);
@@ -67,15 +85,19 @@ public class SelectMenu extends JDialog {
 	public JButton getJbView() {
 		return jbView;
 	}
+
 	public JButton getJbReport() {
 		return jbReport;
 	}
+
 	public JButton getJbLineView() {
 		return jbLineView;
 	}
+
 	public JTextField getJtStart() {
 		return jtStart;
 	}
+
 	public JTextField getJtEnd() {
 		return jtEnd;
 	}
